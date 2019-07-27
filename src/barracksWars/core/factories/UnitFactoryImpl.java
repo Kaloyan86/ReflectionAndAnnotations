309f6ae86482bad5954b4aea6aeed9e6d5af21f0
Kaloyan86 <me4ence3965>
2019-07-27 23:52:08 +0300

    Solution Problem 3 BarracksWars - A New Factory

--- src/barracksWars/core/factories/UnitFactoryImpl.java
+++ src/barracksWars/core/factories/UnitFactoryImpl.java
@@ -4,14 +4,27 @@ import barracksWars.interfaces.Unit;
 import barracksWars.interfaces.UnitFactory;
 import jdk.jshell.spi.ExecutionControl;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class UnitFactoryImpl implements UnitFactory {
 
-	private static final String UNITS_PACKAGE_NAME =
-			"barracksWars.models.units.";
+    private static final String UNITS_PACKAGE_NAME =
+            "barracksWars.models.units.";
 
-	@Override
-	public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
-		// TODO: implement for problem 3
-		throw new ExecutionControl.NotImplementedException("message");
-	}
+    @Override
+    public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
+
+        Unit unit = null;
+        try {
+            Class<?> clazz = Class.forName(UNITS_PACKAGE_NAME + unitType);
+            Constructor ctor = clazz.getDeclaredConstructor();
+            unit = (Unit) ctor.newInstance();
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+        return unit;
+    }
 }
